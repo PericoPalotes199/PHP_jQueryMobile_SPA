e3f383037baa2700bf21d1fad928db62488177c7
Andy Vuong <andy2ndlife@gmail.com>
2019-04-01 11:09:52 -0700

    Update class.database.php

--- core/class.database.php
+++ core/class.database.php
@@ -1,33 +1,21 @@
 <?php
-
 class ManageDatabase {
 
 	protected $db_conn;
 	protected $db_host = 'HOSTNAME';
-    protected $db_name = 'DBNAME';
-    protected $db_user = 'USERNAME';
-    protected $db_pass = 'PASSWORD';
-
-
+	protected $db_name = 'DBNAME';
+	protected $db_user = 'USERNAME';
+	protected $db_pass = 'PASSWORD';
 
 	function connect(){
-
 		try{
-
 			$this->db_conn = new PDO("mysql:host=$this->db_host;dbname=$this->db_name",$this->db_user,$this->db_pass);
-
 			return $this->db_conn;
-
 		}
-
 		catch(PDOException $e){
-
 			return $e->getMessage();
-
 		}
-
 	}
-
 }
 
 ?>

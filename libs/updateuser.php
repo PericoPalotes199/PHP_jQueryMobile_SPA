0cdb2354c5c63864ec40f132ff0a4b04c23ef54b
Andy Vuong <andy2ndlife@gmail.com>
2019-04-01 11:07:22 -0700

    removed ^M character

--- libs/updateuser.php
+++ libs/updateuser.php
@@ -1,20 +1,23 @@
-<?php
-if($_POST){
-	include_once('../core/class.manageUsers.php');
-	$init = new ManageUsers;
-	
-	$userid = trim($_POST['userid']);
-	$param = array('first' => trim($_POST['first']),
-						'last'  => trim($_POST['last']),
-						'username' => trim($_POST['username']),
-						'password' => trim($_POST['password']),
-						'email' => trim($_POST['email']),
-						'userlevel' => 'user');
-	$result = $init->editUsers($userid,$param);
-	if($result >= 0){
-		echo "true";
-	}else{
-		echo $result;
-	}
-}
+<?php
+
+if($_POST){
+	include_once('../core/class.manageUsers.php');
+	$init = new ManageUsers;
+
+	$userid = trim($_POST['userid']);
+	$param = array('first' => trim($_POST['first']),
+						'last'  => trim($_POST['last']),
+						'username' => trim($_POST['username']),
+						'password' => trim($_POST['password']),
+						'email' => trim($_POST['email']),
+						'userlevel' => 'user');
+	$result = $init->editUsers($userid,$param);
+
+	if($result >= 0){
+		echo "true";
+	}else{
+		echo $result;
+	}
+}
+
 ?>

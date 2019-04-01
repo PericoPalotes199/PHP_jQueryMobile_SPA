0aa17db4353608e88327ff8659dfd42bce468a1d
Andy Vuong <andy2ndlife@gmail.com>
2019-04-01 11:05:56 -0700

    removed ^M character

--- libs/deleteuser.php
+++ libs/deleteuser.php
@@ -1,12 +1,14 @@
-<?php
-	include_once('../core/class.manageUsers.php');
-	$init = new ManageUsers;
-
-	$userid = trim($_GET['userid']);
-	$result = $init->deletUsers($userid);
-	if($result == 1){
-		header('location: ../profile.php');
-	}else{
-		echo 'false';
-	}
+<?php
+
+	include_once('../core/class.manageUsers.php');
+	$init = new ManageUsers;
+
+	$userid = trim($_GET['userid']);
+	$result = $init->deletUsers($userid);
+	if($result == 1){
+		header('location: ../profile.php');
+	}else{
+		echo 'false';
+	}
+
 ?>
